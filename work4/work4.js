c34f336ef51ff284494d697b09a7cd90b9dd018e
关一风 <55180779+wins-lcy@users.noreply.github.com>
2019-10-12 12:55:50 +0800

    Update work4.js

--- work4/work4.js
+++ work4/work4.js
@@ -23,7 +23,7 @@ for(let i=1;i<10;++i){
     str += '<tr>'
     for(var j=1;j<=i;++j)[
         //: 拼接单元格
-        str += '</tr>' + j + '*' + i + '=' + (j*i) + '</td>' 
+        str += '<td>' + j + '*' + i + '=' + (j*i) + '</td>' 
     ]
     str += '</tr>'
 }
